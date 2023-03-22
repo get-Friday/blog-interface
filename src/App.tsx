7634b6b51e1d90a0333faad18584478694e292d4
get.Friday <gabriel.getdev@gmail.com>
2023-03-22 17:47:27 -0300

    Feat: Implement useRequest hook and post component on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { BlogPost } from '@components';
+import { useRequest } from '@hooks/useRequest';
+import { POSTS_URL } from '@constants/POSTS_URL';
+import type { IBlogPost } from '@interfaces/IBlogPost';
+
+type GET_BLOG_RESPONSE = IBlogPost;
+
 function App() {
-  return <div className='App'>Hello world</div>;
+  const getAllPostRequest = useRequest<GET_BLOG_RESPONSE[]>(POSTS_URL);
+
+  if (getAllPostRequest.isLoading) {
+    return <main>Carregando</main>;
+  }
+
+  if (getAllPostRequest.error !== undefined) {
+    return <main>Houve um erro ao obter as postagens</main>;
+  }
+
+  return (
+    <main>
+      { getAllPostRequest.response?.map((post: IBlogPost) => 
+        <BlogPost title={post.title} key={post.id}>{ post.body }</BlogPost>
+      )}
+    </main>
+  );
 }
 
 export default App;
