ee77ef30f2ff255cf1b37bb3865bb598b73ed8fb
get.Friday <gabriel.getdev@gmail.com>
2023-03-23 17:41:37 -0300

    Fix: Small style to prevent overflow container with many pages

--- src/components/Paginate/index.tsx
+++ src/components/Paginate/index.tsx
@@ -8,20 +8,22 @@ interface paginateProps {
 
 export function Paginate({ postPerPage, totalPosts, paginate }: paginateProps) {
   const pageNumbers: number[] = [];
-
+  
   if (totalPosts === undefined) {
     return <div>LOADING</div>;
   }
-
+  
   for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
     pageNumbers.push(i);
   }
-
+  
+  const pageNumberClass = `page-number ${(pageNumbers.length > 10) ? 'small' : ''}`
+  
   return (
     <div className='pagination-container'>
       <ul className='pagination'>
         {pageNumbers.map((number) => (
-          <li key={number} className='page-number'>
+          <li key={number} className={pageNumberClass}>
             <button
               onClick={() => {
                 paginate(number);
