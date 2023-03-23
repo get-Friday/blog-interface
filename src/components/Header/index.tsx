9d897923a69962c4085014c973f915797ffd40e5
get.Friday <gabriel.getdev@gmail.com>
2023-03-23 15:26:45 -0300

    Feat: Add title and nav to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
+import './style.css';
+
 export function Header() {
   return (
-    <header>
-      
+    <header className='sticky'>
+      <h2>Blog Legal!</h2>
+      <nav id="blog-content">
+        <div className='nav-option'>Home</div>
+        <a href='https://github.com/get-Friday/blog-interface' className='nav-option' target={'_blank'} rel="noopener noreferrer">About</a>
+      </nav>
     </header>
   )
 }
