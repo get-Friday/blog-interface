a924c74eee20dd118f62496f2f3bd577e9a744ee
get.Friday <gabriel.getdev@gmail.com>
2023-03-23 14:57:11 -0300

    Feat: display data from useRequest on BlogPost

--- src/components/BlogPost/index.tsx
+++ src/components/BlogPost/index.tsx
@@ -1,13 +1,23 @@
+import { USER_URL } from '@constants/USER_URL';
+import { useRequest } from '@hooks/useRequest';
+import type { IUser } from '@interfaces/IUser';
+import './Style.css';
+
 interface postProp {
-  children: string | JSX.Element
-  title: string
+  children: string | JSX.Element;
+  title: string;
+  userId: number;
 }
 
 export function BlogPost(props: postProp) {
+  const getUser = useRequest<IUser>(`${USER_URL}/${props.userId}`);
+
   return (
-    <div>
-      <h1>{ props.title }</h1>
-      { props.children }
+    <div className='post-container'>
+      <h1>{props.title}</h1>
+      <h3>Post made by {getUser.isLoading ? 'loading' : getUser.response?.username}</h3>
+      <p>{props.children}</p>
+      <div className="read-more">READ MORE</div>
     </div>
-  )
+  );
 }
