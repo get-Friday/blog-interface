1ccfbe2d6312bdb0335da908799c0d43a1284402
get.Friday <gabriel.getdev@gmail.com>
2023-03-23 16:53:59 -0300

    Feat: Implement Paginate on App for blog posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { BlogPost, Header } from '@components';
+import { BlogPost, Header, Paginate } from '@components';
 import { useRequest } from '@hooks/useRequest';
 import { POSTS_URL } from '@constants/POSTS_URL';
 import type { IBlogPost } from '@interfaces/IBlogPost';
 import './styles/app.css';
+import { useState } from 'react';
 
 type GET_BLOG_RESPONSE = IBlogPost;
 
 function App() {
   const getAllPostRequest = useRequest<GET_BLOG_RESPONSE[]>(POSTS_URL);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const postPerPage = 10;
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = getAllPostRequest.response?.slice(
+    indexOfFirstPost,
+    indexOfLastPost,
+  );
+
+  function paginate(pageNumber: number) {
+    setCurrentPage(pageNumber);
+  }
 
   if (getAllPostRequest.isLoading) {
     return <main>Loading</main>;
@@ -22,11 +36,16 @@ function App() {
       <Header />
       <main>
         <div className='posts-container'>
-          {getAllPostRequest.response?.map((post: IBlogPost) => (
+          {currentPosts?.map((post: IBlogPost) => (
             <BlogPost title={post.title} userId={post.userId} key={post.id}>
               {post.body}
             </BlogPost>
           ))}
+          <Paginate
+            postPerPage={postPerPage}
+            totalPosts={getAllPostRequest.response?.length}
+            paginate={paginate}
+          />
         </div>
       </main>
     </div>
